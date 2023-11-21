63311d9ab95e83c689dc21a6fb99d5976cabc88a
MrSpaar <pistre.julien@proton.me>
2023-11-21 01:13:09 +0100

    More flexible config

--- build.py
+++ build.py
@@ -7,21 +7,22 @@ NestedDict = dict[str, "str | NestedDict"]
 
 
 class Generator:
-    def __init__(self, src: str, dist: str, templates: str) -> None:
+    def __init__(self, src: str, dist: str, templates: str, assets: str) -> None:
+        self.dist = dist
+        self.assets = assets
         self.url_root = ""
+        self.templates: dict[str, Template] = {}
+        self.structure: NestedDict = self.render_pages(src, f"{dist}/pages")
+
         if 'URL_ROOT' in environ:
-            self.url_root = "/"+environ['URL_ROOT'].split('/')[1]
+            self.url_root = f"/{environ['URL_ROOT'].split('/')[1]}"
 
         if not path.exists(dist):
             makedirs(dist, exist_ok=True)
 
-        self.dist = dist
-        self.structure: NestedDict = self.render_pages(src, dist)
-
         with open("./generated/structure.json", "w") as f:
             dump(self.structure, f, indent=4)
 
-        self.templates: dict[str, Template] = {}
         for file in listdir(templates):
             with open(templates+"/"+file, "r") as f:
                 self.templates[file] = Template(f.read())
@@ -77,27 +78,29 @@ class Generator:
         return nav
     
     def render_nav(self, indents: int) -> str:
-        full_path = self.url_root + self.dist[self.dist.rfind("/")+1:] + "/"
+        full_path = f"{self.url_root}/pages/"
         return self._render_nav(self.structure, full_path, indents, True)
     
-    def render_index(self, indents: int) -> str:
-        return self.render_template(
+    def render_index(self, indents: int) -> None:
+        html = self.render_template(
             "index.html", 0,
             nav=self.render_nav(indents+4), url_root=self.url_root
         )
 
+        with open(f"{self.dist}/index.html", "w") as f:
+            f.write(html)
+
     def link_assets(self) -> None:
         if self.url_root:
-            system("cp -r assets generated/")
-        elif not path.exists("generated/assets"):
-            system("ln -s ../assets generated/")
+            system(f"cp -r {self.assets} {self.dist}/")
+        elif not path.exists(f"{self.dist}/assets"):
+            system(f"ln -s ../{self.assets} {self.dist}/")
 
 
 if __name__ == "__main__":
-    generator = Generator("pages", "./generated/pages", "templates")
-
-    with open("./generated/index.html", "w") as f:
-        f.write(generator.render_index(8))
+    generator = Generator(
+        "pages", "generated", "templates", "assets"
+    )
 
     generator.link_assets()
-    
+    generator.render_index(4)
