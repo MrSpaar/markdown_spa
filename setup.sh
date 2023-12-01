aaf4f5bc32fa50e6e719a008938f5a0d71cb56f9
MrSpaar <pistre.julien@proton.me>
2023-12-01 02:40:11 +0100

    Safer setup script

--- setup.sh
+++ setup.sh
@@ -1,13 +1,41 @@
-LIGHT_BLUE='\033[1;34m'
-GREEN='\033[0;32m'
+#!/bin/bash
+set -e
+
 NC='\033[0m'
 BOLD='\033[1m'
+RED='\033[0;31m'
+BLUE='\033[1;34m'
+GREEN='\033[0;32m'
 
-echo -ne "${LIGHT_BLUE}${BOLD}Enter where to setup the project (blank for current directory): ${NC}"
+if ! command -v git &>/dev/null; then
+    echo -e "${RED}${BOLD}Git not found. Please install git.${NC}"
+    exit 1
+fi
+
+if command -v python &>/dev/null; then
+    python=python
+elif command -v python3 &>/dev/null; then
+    python=python3
+else
+    echo -e "${RED}${BOLD}Python not found. Please install python 3.6 or higher.${NC}"
+    exit 1
+fi
+
+if command -v pip &>/dev/null; then
+    pip=pip
+elif command -v pip3 &>/dev/null; then
+    pip=pip3
+elif command -v python -m pip &>/dev/null; then
+    pip=python -m pip
+else
+    echo -e "${RED}${BOLD}Pip not found. Please install pip.${NC}"
+    exit 1
+fi
+
+echo -ne "${BLUE}${BOLD}Enter where to setup the project (blank for current directory): ${NC}"
 read -r
 
-if [[ $REPLY != "" ]]
-then
+if [[ $REPLY != "" ]]; then
     mkdir -p $REPLY
     cd $REPLY
 fi
@@ -19,29 +47,35 @@ echo "name: Main Page
 # Hello World!" > pages/index.md
 git init
 
-echo -ne "${LIGHT_BLUE}${BOLD}Create a remote branch to your github repo? (Y/n)${NC} "
+echo -ne "${BLUE}${BOLD}Create a remote branch to your github repo? (Y/n)${NC} "
 read -n 1 -r
 echo
 
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
+if [[ $REPLY =~ ^[Yy]$ ]]; then
     echo
-    echo -ne "${LIGHT_BLUE}${BOLD}Enter the name of the remote branch: ${NC}"
+    echo -ne "${BLUE}${BOLD}Enter the name of the remote branch: ${NC}"
     read -r
 
     git remote add origin $REPLY
 fi
 
 
-python -m pip install markdown jinja2 pygments libsass
-
-echo -ne "${LIGHT_BLUE}${BOLD}Install watchdog (test server + file watcher)? (Y/n)${NC} "
+echo -ne "${BLUE}${BOLD}Install dependencies? (Y/n)${NC} "
 read -n 1 -r
-
 echo
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-    python -m pip install watchdog
+
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    $pip install jinja2 markdown Pygments
+
+    echo -ne "${BLUE}${BOLD}Install watchdog (test server + file watcher)? (Y/n)${NC} "
+    read -n 1 -r
+    echo
+
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        $pip install watchdog
+    fi
+
+    echo -e "${GREEN}${BOLD}Dependencies installed.${NC}"
 fi
 
-echo -e "${GREEN}${BOLD}Setup complete. Use ${LIGHT_BLUE}${BOLD}'python -m build'${GREEN}${BOLD} to build the site or ${LIGHT_BLUE}${BOLD}'python watch.py'${GREEN}${BOLD} to start a local server.${NC}"
+echo -e "${GREEN}${BOLD}Setup complete. Use ${BLUE}${BOLD}'python -m build'${GREEN}${BOLD} to build the site or ${BLUE}${BOLD}'python watch.py'${GREEN}${BOLD} to start a local server.${NC}"
