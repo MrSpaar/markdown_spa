733822249724a05240524317fe33513db0a38a79
MrSpaar <pistre.julien@proton.me>
2023-12-23 02:44:26 +0100

    Using flit to build & automatic versioning

--- src/markdown_spa/__init__.py
+++ src/markdown_spa/__init__.py
@@ -1,8 +1,11 @@
+from os import environ
+
 from .cli import main
 from .generator import Generator, Extension
 from .extensions import SASS, Sitemap, Tailwind
 
 __all__ = ["Generator", "Extension", "SASS", "Sitemap", "Tailwind"]
+__version__ = environ.get("MARKDOWN_SPA_VERSION", '0.0.0').removeprefix("v")
 
 if __name__ == "__main__":
     main()
