f7d89a0a21160461e1ff1d47707c66286e294ceb
MrSpaar <pistre.julien@proton.me>
2023-12-23 16:39:32 +0100

    Removed print

--- src/markdown_spa/__init__.py
+++ src/markdown_spa/__init__.py
@@ -5,14 +5,13 @@ from .cli import main
 from .generator import Generator, Extension
 from .extensions import SASS, Sitemap, Tailwind
 
-if "MARKDOWN_SPA_VERSION" in environ:
-    print(environ["MARKDOWN_SPA_VERSION"])
 
 __all__ = ["Generator", "Extension", "SASS", "Sitemap", "Tailwind"]
 __version__ = environ.get(
     "MARKDOWN_SPA_VERSION",
     get("https://pypi.org/pypi/markdown-spa/json").json()["info"]["version"]
 ).removeprefix("v")
+
 
 if __name__ == "__main__":
     main()
