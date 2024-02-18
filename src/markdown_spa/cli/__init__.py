b57f9047308b2055b06027dcba3c4147f277fd77
MrSpaar <julien.pistre@icloud.com>
2024-02-18 23:37:48 +0100

    Removed useless imports

--- src/markdown_spa/cli/__init__.py
+++ src/markdown_spa/cli/__init__.py
@@ -1,11 +1,7 @@
 from .project import init, watch, build
 from .extensions import install, uninstall, add, list
 
-from requests import get
-from sys import executable
-from importlib_metadata import version
-
-from click import group, secho
+from click import group
 
 
 @group(context_settings=dict(help_option_names=['-h', '--help']))
